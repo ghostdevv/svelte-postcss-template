b7564c5f981f4964a779ef7413e41ad4e79b3d5d
GHOST <ghostdevbusiness@gmail.com>
2020-12-07 12:19:45 +0000

    feat: Updated boilerplate to be simpler

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,16 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello World!</h1>
 </main>
 
 <style>
 	main {
 		text-align: center;
+		display: flex;
+		justify-content: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
